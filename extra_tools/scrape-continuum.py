4b94e80a55ca709579fafe998dd4d09a33500fda
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-03-27 00:20:32 +0000

    we forgot some metadata from the continuum notebook

--- extra_tools/scrape-continuum.py
+++ extra_tools/scrape-continuum.py
@@ -23,18 +23,21 @@ def freeze_dd(dd):
     return result
 
 name_map = {
-    "Pseudoscalar": "PS",
-    "Vector": "V",
-    "Axial-vector": "AV",
-    "Axial-tensor": "AT",
-    "Scalar": "S",
-    "Tensor": "T",
-    "mass": "mass",
-    "decay": "decayconst",
-    "AS": "Antisymmetric",
-    "S": "Symmetric",
-    "F": "Fundamental",
+    "Pseudoscalar": "pseudoscalar",
+    "Vector": "vector",
+    "Axial-vector": "axialvector",
+    "Axial-tensor": "axialtensor",
+    "Scalar": "scalar",
+    "Tensor": "tensor",
+    "mass": "masses",
+    "decay": "decayconsts",
+    "AS": "AS",
+    "S": "S",
+    "F": "F",
 }
+
+def get_metadata_node(Nc, rep, channel, observable):
+    return metadata[f"Sp{Nc}"][name_map[rep]][name_map[channel]][name_map[observable]]
 
 for line in stripreader("notebooks_cat"):
     if line.endswith('"Section",') and len(split_line := line.split('"')[1].split()) > 1:
@@ -45,12 +48,18 @@ for line in stripreader("notebooks_cat"):
         get_seed = True
     elif line.startswith('RowBox[{"Rep", ":="'):
         rep = line.split('"')[6].strip(r"\<>")
+    elif line.startswith('RowBox[{"minmassPS", "=",'):
+        minmassPS = float(line.split('"')[5])
+        get_metadata_node(Nc, rep, channel, observable)["minmassPS"] = minmassPS
+    elif line.startswith('RowBox[{"maxmassPS", "=",'):
+        minmassPS = float(line.split('"')[5])
+        get_metadata_node(Nc, rep, channel, observable)["maxmassPS"] = minmassPS
     elif get_seed:
         get_seed = False
         seed = int(line.split('"')[1])
         if Nc == 8:
             seed = randint(0, 32768)
-        metadata[f"Sp{Nc}"][name_map[rep]][name_map[channel]][name_map[observable]]["seed"] = seed
+        get_metadata_node(Nc, rep, channel, observable)["seed"] = seed
 
 metadata = freeze_dd(metadata)
 with open("continuum.yaml", "w") as f:
