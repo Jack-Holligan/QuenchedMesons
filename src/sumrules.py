Jack-Holligan/QuenchedMesons

commit d20ec3ab4bec47661d73b47415bdaa23fac12756

fix obvious bugs in previously untested sum rule code and make it output a csv as well

src/sumrules.py
@@ -1,37 +1,51 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
+import csv
 import math
 from sys import stdout
 from uncertainties import ufloat
 
 Ncs = [4, 6, 8]
-reps = ["F", "AS", "S"]
+reps = {"F": "fundamental", "AS": "antisymmetric", "S": "symmetric"}
 observables = ["masses", "decayconsts"]
 
 
-def s0(vector_decay_const, vector_mass, axialvector_decay_const, axialvector_mass):
+def value_or_none(func):
+    def wrapped_function(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except TypeError:
+            return None
+
+    return wrapped_function
+
+
+@value_or_none
+def s0(
+    vector_decayconsts, vector_masses, axialvector_decayconsts, axialvector_masses, **_
+):
     return (
         4
         * math.pi
         * (
-            vector_decay_const_square / vector_mass_square
-            - axialvector_decay_const_square / axialvector_mass_square
+            vector_decayconsts / vector_masses
+            - axialvector_decayconsts / axialvector_masses
         )
     )
 
 
-def s1(vector_decay_const, axialvector_decay_const, pseudoscalar_decay_const):
-    return (
-        1
-        - (axialvector_decay_const_square + pseudoscalar_decay_const_square)
-        / vector_decay_const_square
-    )
+@value_or_none
+def s1(vector_decayconsts, axialvector_decayconsts, pseudoscalar_decayconsts, **_):
+    return 1 - (axialvector_decayconsts + pseudoscalar_decayconsts) / vector_decayconsts
 
 
-def s2(vector_decay_const, vector_mass, axialvector_decay_const, axialvector_mass):
-    return 1 - axialvector_mass_square * axialvector_decay_const_square / (
-        vector_mass_square * vector_decay_const_square
+@value_or_none
+def s2(
+    vector_decayconsts, vector_masses, axialvector_decayconsts, axialvector_masses, **_
+):
+    return 1 - axialvector_masses * axialvector_decayconsts / (
+        vector_masses * vector_decayconsts
     )
 
 
@@ -39,10 +53,20 @@ s_functions = {"s0": s0, "s1": s1, "s2": s2}
 
 
 def get_single_N_rep_data(Nc, rep, channel_observable):
-    with open(f"processed_data/Sp{Nc}/{rep}/{channel_observable}.dat", "r") as f:
-        if len(lines := f.readlines()) == 0 or len(split_line := lines[0].split()) < 2:
-            return
-        return ufloat(*map(float, split_line))
+    try:
+        with open(
+            f"processed_data/Sp{Nc}/continuum/{rep}/{channel_observable}_{rep}_Sp{Nc}.dat",
+            "r",
+        ) as f:
+            if (
+                len(lines := f.readlines()) == 0
+                or len(split_line := lines[0].split()) < 2
+            ):
+                return
+    except FileNotFoundError:
+        return
+
+    return ufloat(*map(float, split_line))
 
 
 def get_finite_N_data():
@@ -56,7 +80,7 @@ def get_finite_N_data():
     return {
         Nc: {
             rep: {
-                channel_observable: get_single_file_data(Nc, rep, channel_observable)
+                channel_observable: get_single_N_rep_data(Nc, rep, channel_observable)
                 for channel_observable in channel_observables
             }
             for rep in reps
@@ -91,23 +115,31 @@ def get_args():
     from sys import stdout
 
     parser = ArgumentParser()
-    parser.add_argument("--output_file", default=stdout, type=FileType("w"))
+    parser.add_argument("--output_table", default=stdout, type=FileType("w"))
+    parser.add_argument("--output_csv", default=None, type=FileType("w"))
     return parser.parse_args()
 
 
 def print_single(label, s_data, s0_slug="", output_file=stdout):
-    line_format = (
-        r"{label}, ({rep}) & ${s0:.02uSL}$ {s0_slug} & ${s1:.02uSL}$ & ${s2:.02uSL}$ \\"
-    )
+    line_format = r"{label}"
+    for s_label, s_datum in s_data.items():
+        if s_datum is not None:
+            line_format += f" & ${{{s_label}:.02uSL}}$"
+            if s_label == "s0":
+                line_format += f" {{{s_label}_slug}}"
+        else:
+            line_format += r" & $\cdots$"
+    line_format += r" \\"
+
     print(line_format.format(label=label, s0_slug=s0_slug, **s_data), file=output_file)
 
 
 def print_bunches(data, slugs=defaultdict(lambda: ""), output_file=stdout):
-    for Nc_label, Nc_data in data.items():
+    for Nc, Nc_data in data.items():
         print(r"\hline", file=output_file)
         for rep, s_data in Nc_data.items():
             print_single(
-                f"{Nc_label}, ({rep.lower()})",
+                f"$Sp({Nc})$, ({reps[rep].lower()})",
                 s_data,
                 s0_slug=slugs[rep],
                 output_file=output_file,
@@ -135,7 +167,53 @@ def compute_single_s(datum):
 
 
 def compute_s(data):
-    return {rep: compute_single_s(datum) for rep, datum in data.items()}
+    return {
+        rep: compute_single_s(datum) if datum else None for rep, datum in data.items()
+    }
+
+
+def split_ufloat(prefix, data):
+    return {
+        f"{prefix}_value": data.nominal_value if data else None,
+        f"{prefix}_uncertainty": data.std_dev if data else None,
+    }
+
+
+def write_csv_row(group_family, Nc, representation, Nf, data, writer):
+    to_write = {
+        "group_family": group_family,
+        "Nc": Nc,
+        "representation": representation,
+        "Nf": Nf,
+    }
+    for s in "s0", "s1", "s2":
+        to_write.update(**split_ufloat(s, data[s]))
+
+    writer.writerow(to_write)
+
+
+def output_csv(finite_N, large_N, su3, output_file):
+    writer = csv.DictWriter(
+        output_file,
+        fieldnames=[
+            "group_family",
+            "Nc",
+            "representation",
+            "Nf",
+            "s0_value",
+            "s0_uncertainty",
+            "s1_value",
+            "s1_uncertainty",
+            "s2_value",
+            "s2_uncertainty",
+        ],
+    )
+    writer.writeheader()
+    for Nc, Nc_data in {**finite_N, **large_N}.items():
+        for representation, rep_data in Nc_data.items():
+            write_csv_row("Sp", Nc, representation, 0, rep_data, writer)
+
+    write_csv_row("SU", 3, "F", 2, su3, writer)
 
 
 def main():
@@ -153,17 +231,24 @@ def main():
     }
 
     finite_N_results = {
-        f"$Sp({Nc})$": compute_s(data) for Nc, data in finite_N_squared_data.items()
+        Nc: compute_s(data) for Nc, data in finite_N_squared_data.items()
     }
-    large_N_results = {r"$Sp(\infty)$": compute_s(large_N_squared_data)}
+    large_N_results = {r"\infty": compute_s(large_N_squared_data)}
     su3_results = {
         r"$SU(3)$, $N_{(\mathrm{f})}=2$ ($m_\pi=139.6\textnormal{ MeV}$)": compute_single_s(
-            su3_squared_data
+            compute_single_s(su3_squared_data)
         )
     }
     output_table(
-        finite_N_results, large_N_results, su3_results, output_file=args.output_file
+        finite_N_results, large_N_results, su3_results, output_file=args.output_table
     )
+    if args.output_csv:
+        output_csv(
+            finite_N_results,
+            large_N_results,
+            compute_single_s(su3_squared_data),
+            output_file=args.output_csv,
+        )
 
 
 if __name__ == "__main__":

Snakefile
@@ -485,11 +485,12 @@ rule sum_rules:
         large_N_data=large_N_table_inputs,
         finite_N_data=contlim_table_inputs,
     output:
-        "tables/sumrules.tex",
+        table="tables/sumrules.tex",
+        csv="csvs/sumrules.csv",
     conda:
         "environment.yml"
     shell:
-        "python {input.script} --output_file {output}"
+        "python {input.script} --output_table {output.table} --output_csv {output.csv}"
 
 
 def ensemble_masses():

commit 1727d806469908546a0a71487c1a4386d56f9d12

revert obvious bug introduced in d20ec3a while fixing other obvious bugs

src/sumrules.py
@@ -242,7 +242,7 @@ def main():
     }
     large_N_results = {r"\infty": compute_s(large_N_squared_data)}
     su3_results = {
-        r"$SU(3)$, $N_{(\mathrm{f})}=2$ ($m_\pi=139.6\textnormal{ MeV}$)": compute_single_s(
+        r"$SU(3)$, $N_{(\mathrm{f})}=2$ ($m_\pi=139.6\textnormal{ MeV}$)": (
             compute_single_s(su3_squared_data)
         )
     }